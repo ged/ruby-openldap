ceb30935d0df2a5b9567f004620613180ead0d7b
Michael Granger <ged@FaerieMUD.org>
2011-02-11 16:38:56 -0800

    Initial project skeleton generated by Hoe

---
+++ lib/openldap.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# FIX (top-level documentation)
+# 
+# @author Michael Granger <ged@FaerieMUD.org>
+# 
+module Openldap
+
+	# Library version constant
+	VERSION = '0.0.1'
+
+	# Version-control revision constant
+	REVISION = %q$Revision$
+
+end # module Openldap
+
